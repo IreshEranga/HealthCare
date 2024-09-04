5900314fa22b05cd05fdad7162b86c748e4bb71a
Iresh Eranga <iresheranga2020al@gmail.com>
2024-09-04 14:28:54 +0530

    Create LogIn.jsx

---
+++ frontend/app/LogIn.jsx
@@ -0,0 +1,14 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class LogIn extends Component {
+  render() {
+    return (
+      <View>
+        <Text>LogIn</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
