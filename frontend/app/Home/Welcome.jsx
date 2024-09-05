55469063454569d088826bdb50a054f3f61fcf58
Iresh Eranga <iresheranga2020al@gmail.com>
2024-09-05 20:06:46 +0530

    Update Welcome.jsx
    add greeting

--- frontend/app/Home/Welcome.jsx
+++ frontend/app/Home/Welcome.jsx
@@ -2,11 +2,39 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function Welcome() {
+
+  const currentHour = new Date().getHours();
+
+  let greetingMessage = '';
+
+  if (currentHour < 12){
+    greetingMessage = 'Good Morning';
+  } else if (currentHour < 18){
+    greetingMessage = 'Good Afternoon';
+  } else{
+    greetingMessage = 'Good Night';
+  }
+
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <Text style={styles.hometext}>Home</Text>
+      <Text style={styles.greetingMessage}>{greetingMessage}</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  hometext:{
+    textAlign:'center',
+    fontWeight:'bold',
+    fontSize:30,
+    marginTop:20
+  },
+  greetingMessage:{
+    textAlign:'left',
+    fontSize:20,
+    marginTop:10,
+    marginLeft:25,
+  }
+})
