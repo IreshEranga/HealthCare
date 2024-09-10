148e225c3e8960cf531d218c75cb09956d587f0b
Iresh Eranga <iresheranga2020al@gmail.com>
2024-09-10 19:55:44 +0530

    Update babel.config.js

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,6 +1,19 @@
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo'],
+//   };
+// };
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['module:react-native-dotenv', {
+        moduleName: '@env', // This is where you'll import from
+        path: '.env', // This is the path to your .env file
+      }]
+    ],
   };
 };
