b2d3373573749080bdd1f79b6de7bd54ef711671
shenori <it22150448@my.sliit.lk>
2024-09-15 23:39:42 +0530

    implement food list ui

---
+++ frontend/app/Nutritions/nutritionHome.tsx
@@ -0,0 +1,56 @@
+import { Image, StyleSheet, Platform, Text, View, FlatList, TextInput, Button } from 'react-native';
+import FoodListItem from '@/Components/FoodListItem';
+import { useState } from 'react';
+
+const foodItems = [
+  {label: 'Pizza', cal: 75, brand: 'Dominos'},
+  {label: 'Pasta', cal: 125, brand: 'Italian'},
+  {label: 'Noodles', cal: 92, brand: 'Ramen'},
+]
+
+export default function HomeScreen() {
+  const [search, setSearch] = useState('');
+
+  const performSearch = () => {
+    console.warn('Searching for: ', search);
+
+    setSearch('');
+
+  }
+
+  return (
+    <View style={styles.container}>
+
+      <TextInput 
+        value={search} 
+        onChangeText={setSearch} 
+        placeholder="Search..." 
+        style={styles.input} 
+      />
+
+      {search&& <Button title='Search' onPress={performSearch} />}
+
+      <FlatList
+      data={foodItems}
+      renderItem={({ item }) => <FoodListItem item={item}/>}
+      contentContainerStyle={{ gap: 5}}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 10,
+    gap: 15,
+    marginTop: 40
+  },
+
+  input: {
+    backgroundColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 25
+  }
+});
