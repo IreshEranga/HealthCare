a13f5192295741081eafdfaa4037634212e57212
Iresh Eranga <iresheranga2020al@gmail.com>
2024-09-16 17:35:50 +0530

    Create GoalActivity.jsx

---
+++ frontend/app/PersonalizeMentalGoals/GoalActivity.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function GoalActivity() {
+  return (
+    <View>
+      <Text>GoalActivity</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
