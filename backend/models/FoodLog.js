2ff43a3aad2b52abf25e031fb6d5092e727beb27
shenori <it22150448@my.sliit.lk>
2024-09-21 00:05:06 +0530

    create foodlog model

---
+++ backend/models/FoodLog.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const foodLogSchema = new mongoose.Schema({
+  userID: {
+    type: String,
+    required: true,
+  },
+  date: { 
+    type: String, 
+    required: true 
+  },
+  mealType: { 
+    type: String, 
+    required: true 
+  },
+  foodItems: [
+    {
+      name: { type: String, 
+        required: true 
+      },
+      calories: { 
+        type: Number, 
+        required: true 
+      },
+    },
+  ],
+});
+
+const FoodLog = mongoose.model('FoodLog', foodLogSchema);
+
+module.exports = FoodLog;
