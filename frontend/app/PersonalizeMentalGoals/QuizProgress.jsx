afc488c34cae30b5caefd82c3efa98f5dc245fa3
Iresh Eranga <iresheranga2020al@gmail.com>
2024-10-08 09:21:24 +0530

    Update QuizProgress.jsx

--- frontend/app/PersonalizeMentalGoals/QuizProgress.jsx
+++ frontend/app/PersonalizeMentalGoals/QuizProgress.jsx
@@ -1,12 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useNavigation } from '@react-navigation/native';
+import NavBar from '../../components/NavBar';
 
 export default function QuizProgress() {
   return (
-    <View>
-      <Text>QuizProgress</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <LinearGradient
+        colors={['#E0BBE4', '#aec2b6', '#60768d']}
+        style={styles.background}
+      >
+        <ScrollView style={styles.scrollContainer}>
+            
+          
+        </ScrollView>
+        <NavBar />
+      </LinearGradient>
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#0e1138',
+        flex: 1,
+      },
+      background: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        height: '100%',
+      },
+      scrollContainer: {
+        marginTop: 10,
+        marginHorizontal: 20,
+      },
+})
