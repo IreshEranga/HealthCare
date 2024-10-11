67a53d03845fa0944c76af73f1c90ccdd240893f
Iresh Eranga <iresheranga2020al@gmail.com>
2024-10-11 12:13:46 +0530

    Update UserController.js

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -36,4 +36,97 @@ const createUser = async(req,res) => {
   }
 };
 
-module.exports = { createUser };
+// Get user details by user ID
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+// Update user details
+const updateUserDetails = async (req, res) => {
+  const { id } = req.params;
+  const { first_name, last_name, email, mobile, profession, gender } = req.body;
+
+  try {
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Update user details
+    user.first_name = first_name || user.first_name;
+    user.last_name = last_name || user.last_name;
+    user.email = email || user.email;
+    user.mobile = mobile || user.mobile;
+    user.profession = profession || user.profession;
+    user.gender = gender || user.gender;
+
+    await user.save();
+    res.status(200).json({ message: "User details updated successfully", user });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const updateUserTypeToPremium = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Update user type to 'premium'
+    user.type = 'premium';
+
+    await user.save();
+    res.status(200).json({ message: "User type updated to premium successfully", user });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+const getUserType = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    // Find the user by ID
+    const user = await User.findById(id);
+
+    // Check if the user exists
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Return the user's type
+    res.status(200).json({ type: user.type });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+
+module.exports = { createUser,getUserById, 
+  updateUserDetails, 
+  updateUserTypeToPremium ,
+  getUserType};
